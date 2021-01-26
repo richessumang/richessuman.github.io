e9072d6ad183b6b81b1cbb8408fa92a943072dc6
richessumang <ess18001@byui.edu>
2021-01-26 05:13:28 +0200

    commit

---
+++ week1/script.js
@@ -0,0 +1,30 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let monthes = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = monthes[d.getMonth()];
+let year =  d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+document.getElementById("currentdate").textContent = fulldate;
